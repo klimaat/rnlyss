e2f53c27976e7158ec9177910a139bd5abe47f4f
Michael Roth <michael.j.roth@gmail.com>
2021-11-05 13:02:51 -0400

    Added area method and some additional bounds checking

--- rnlyss/grid.py
+++ rnlyss/grid.py
@@ -4,6 +4,8 @@
 
 import numpy as np
 
+_EARTH_RADIUS = 6370000.0
+
 
 def center(lon, rad=False):
     """
@@ -22,6 +24,8 @@ class Grid(object):
         origin=(-90, -180),
         delta=(1, 1),
         periodic=True,
+        pole_to_pole=True,
+        r=_EARTH_RADIUS,
         **kwargs
     ):
 
@@ -29,10 +33,14 @@ class Grid(object):
         self.origin = origin
         self.delta = delta
         self.periodic = periodic
+        self.pole_to_pole = pole_to_pole
 
         # Default coordinates in map'd space
         self.y0, self.x0 = origin
         self.dy, self.dx = delta
+
+        # Radius of Earth (m)
+        self.r = r
 
     def __getitem__(self, args):
         """
@@ -45,12 +53,12 @@ class Grid(object):
         # Convert to lat, lon
         lat, lon = self.xy2ll(x, y)
 
-        if np.isscalar(args[0]) and np.isscalar(args[1]):
-            return np.asscalar(lat), np.asscalar(lon)
-        else:
-            return lat, lon
+        return (
+            np.asscalar(lat) if np.isscalar(args[0]) else lat,
+            np.asscalar(lon) if np.isscalar(args[1]) else lon,
+        )
 
-    def __call__(self, lat, lon, snap=False):
+    def __call__(self, lat, lon, snap=False, limit=False):
         """
         Return location in grid given latitude, longitude.
         """
@@ -61,26 +69,40 @@ class Grid(object):
         # Find i, j
         i, j = self.xy2ij(x, y)
 
-        if snap:
-            i, j = np.rint(i).astype(np.int), np.rint(j).astype(np.int)
-
         if self.periodic:
             j %= self.shape[1]
 
-        if np.isscalar(lat) and np.isscalar(lon):
-            return np.asscalar(i), np.asscalar(j)
-        else:
-            return i, j
+        if limit:
+            eps = np.finfo(i.dtype).eps
+            i = np.clip(i, -0.5 + eps, self.shape[0] - 0.5 - eps)
+            j = np.clip(j, -0.5 + eps, self.shape[1] - 0.5 - eps)
+
+        if snap:
+            i, j = np.rint(i).astype(np.int), np.rint(j).astype(np.int)
+
+        return (
+            np.asscalar(i) if np.isscalar(lat) else i,
+            np.asscalar(j) if np.isscalar(lon) else j,
+        )
 
     def lats(self):
+        # These should be consistent with Gaussian... i.e. method vs. value
         return self[np.arange(self.shape[0]), 0][0]
 
     def lons(self):
         return self[0, np.arange(self.shape[1])][1]
 
+    def indices(self):
+        """
+        Return a full set of i, j indices
+        """
+        return np.meshgrid(
+            np.arange(self.shape[0]), np.arange(self.shape[1]), indexing="ij"
+        )
+
     def bbox(self, lat, lon):
         """
-        Return indices and weights of corners of bbox given lat, lon.
+        Return indices and weights of (i, j) vertices of grid given lat, lon.
 
         Handles a few tricky edge cases:
         - extrapolation if outside data (e.g. Gaussian latitudes)
@@ -142,11 +164,61 @@ class Grid(object):
         """
         Default projection lat=y, lon=x
         """
-        return y, center(x)
+        return np.clip(y, -90, 90), center(x)
+
+    def cell_corners(self, lat, lon):
+        """
+        Return latitudes of cell centered at (lat, lon)
+
+        Nominally this is (lat ± dlat/2, lon ± dlon/2) but have to check
+        limits
+
+        Don't apply periodic boundary modulus but *do* clip latitudes
+        """
+        return [
+            (np.clip(lat + dlat, -90, 90), lon + dlon)
+            for dlat in [-self.dy / 2, self.dy / 2]
+            for dlon in [-self.dx / 2, self.dx / 2]
+        ]
+
+    def areas(self, r=None):
+        """
+        Return *all* areas in matrix ni x nj matrix
+        """
+
+        if r is None:
+            r = self.r
+
+        # Longitude constant
+        dlon = np.abs(self.dx)
+
+        # Latitudes could change based on limits or Gaussian
+        lats = self[np.arange(self.shape[0] + 1) - 0.5, 0][0]
+
+        # If grid spans pole-to-pole, force latitude limits
+        if self.pole_to_pole:
+            lats[lats.argmax()] = 90.0
+            lats[lats.argmin()] = -90
+
+        # Center of cell (not necessarily where cell stored)
+        latc = (lats[:-1] + lats[1:]) / 2
+        dlat = np.abs(np.diff(lats))
+
+        # Calculate areas
+        areas = (
+            np.radians(dlon)
+            * 2
+            * np.cos(np.radians(latc))
+            * np.sin(np.radians(dlat) / 2)
+            * r ** 2
+        )
+
+        # Copy in longitude dir'n
+        return np.repeat(areas[:, None], self.shape[1], 1)
 
     def extents(self):
         """
-        Return latitude and longitudes of corners.
+        Return latitude and longitudes of corner cells.
         """
         i = [0, 0, self.shape[0] - 1, self.shape[0] - 1]
         j = [0, self.shape[1] - 1, self.shape[1] - 1, 0]
@@ -217,9 +289,26 @@ class GaussianGrid(Grid):
 
 def test():
 
+    # Regular boring grid
+    grid = Grid(shape=(181, 360), origin=(-90, -180), delta=(1, 1))
+    A = grid.areas(r=1.0)
+    print(A.shape, np.sum(A)/(4*np.pi))
+
+    # MERRA-2
     grid = Grid(shape=(361, 576), origin=(-90, -180), delta=(1 / 2, 5 / 8))
-    print(grid.lats())
-    print(grid.lons())
+    A = grid.areas(r=1.0)
+    print(A.shape, np.sum(A)/(4*np.pi))
+
+    # ERA5
+    # NB. Data downloaded from Copernicus in netCDF4 format is 0.25°×0.25°
+    grid = Grid(shape=(721, 1440), origin=(90, 0), delta=(-1 / 4, 1 / 4))
+    A = grid.areas(r=1.0)
+    print(A.shape, np.sum(A) / (4 * np.pi))
+
+    # CFSv2
+    grid = GaussianGrid(shape=(880, 1760), origin=(90, 0), delta=(-1, 360 / 1760))
+    A = grid.areas(r=1.0)
+    print(A.shape, np.sum(A) / (4 * np.pi))
 
 
 if __name__ == "__main__":
